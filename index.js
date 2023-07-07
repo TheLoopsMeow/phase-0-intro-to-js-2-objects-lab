11a8135e6574a33ba701aae2335aff939cba1bc2
Wyatt <wyattstephenson@rocketmail.com>
2023-07-07 11:37:15 -0600

    Intro to JS 2 Objects Lab completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Tommy Oliver",
+    address: "40 Oak Street",
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const anotherEmployee = { ... obj};
+    anotherEmployee[key] = value;
+    return anotherEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const anotherEmployee = { ...obj };
+    delete anotherEmployee[key];
+    return anotherEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
+
+destructivelyDeleteFromEmployeeByKey(employee, address);
